2236072ad7cd0d1c8ba1eabe9ec0d5b324939214
Harry Santoso <harrysantoso8@yahoo.com>
2024-10-03 21:00:13 +0700

    model, factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Book;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +21,35 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+
+            // laravel handle the book id
+            Review::factory()->count($numReviews)
+                ->good()
+                ->for($book)
+                ->create();
+        });
+
+        Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+
+            // laravel handle the book id
+            Review::factory()->count($numReviews)
+                ->average()
+                ->for($book)
+                ->create();
+        });
+
+        Book::factory(34)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+
+            // laravel handle the book id
+            Review::factory()->count($numReviews)
+                ->bad()
+                ->for($book)
+                ->create();
+        });
     }
 }
