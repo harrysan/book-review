f68158be20f43d9b0cfda18e5c416daa92d769d6
Harry Santoso <harrysantoso8@yahoo.com>
2024-10-07 19:24:40 +0700

    create review and rate limiting

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Support\Facades\Cache;
 
 class Book extends Model
 {
@@ -21,19 +22,29 @@ class Book extends Model
         return $query->where('title', 'LIKE', '%' . $title . '%');
     }
 
-    public function scopePopular(Builder $query, $from = null, $to = null): Builder | QueryBuilder
+    public function scopeWithReviewsCount(Builder $query, $from = null, $to = null): Builder | QueryBuilder
     {
         return $query->withCount([
             'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
-        ])
+        ]);
+    }
+
+    public function scopeWithAvgRating(Builder $query, $from = null, $to = null): Builder | QueryBuilder
+    {
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating');
+    }
+
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder | QueryBuilder
+    {
+        return $query->withReviewsCount()
             ->orderBy('reviews_count', 'desc');
     }
 
     public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder | QueryBuilder
     {
-        return $query->withAvg([
-            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
-        ], 'rating')
+        return $query->withAvgRating()
             ->orderBy('reviews_avg_rating', 'desc');
     }
 
@@ -79,5 +90,12 @@ class Book extends Model
         return $query->highestRated(now()->subMonths(6), now())
             ->popular(now()->subMonths(6), now())
             ->minReviews(5);
+    }
+
+    protected static function booted()
+    {
+        // when review is modified then run forget cache
+        static::updated(fn(Book $book) => Cache::forget('book:' . $book->id));
+        static::deleted(fn(Book $book) => Cache::forget('book:' . $book->id));
     }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -22,5 +22,6 @@ class Review extends Model
         // when review is modified then run forget cache
         static::updated(fn(Review $review) => Cache::forget('book:' . $review->book_id));
         static::deleted(fn(Review $review) => Cache::forget('book:' . $review->book_id));
+        static::created(fn(Review $review) => Cache::forget('book:' . $review->book_id));
     }
 }
